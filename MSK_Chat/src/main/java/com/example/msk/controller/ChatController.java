5d8cb6728869d6c3ee630699db95342754376f51
MikkelKarlsson <Mikkel.karlsson@hotmail.com>
2020-11-24 13:07:00 +0100

    ting

--- MSK_Chat/src/main/java/com/example/msk/controller/ChatController.java
+++ MSK_Chat/src/main/java/com/example/msk/controller/ChatController.java
@@ -6,9 +6,24 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class ChatController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/loginpage")
+    public String loginpage (Model model) {
+        return "loginpage";
+    }
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
